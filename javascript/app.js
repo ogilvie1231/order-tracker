d19d50752cfd33ae97b3f4d9a05df3c1270ffb00
ogilvie1231 <ogilvie1231@gmail.com>
2020-07-21 07:34:25 -0600

    setting up logic to track totals

--- javascript/app.js
+++ javascript/app.js
@@ -35,64 +35,73 @@ $("#new-order-btn").on("click", function (event) {
       date,
       complete: false,
     };
-    console.log("newOrder: ", newOrder);
+  
     database.ref().push(newOrder);
 
-    //Eventually add 'window.location.reload();'
+    // window.location.reload();
     $("#vendor").val("");
     $("#cost").val("");
     $("#date-placed").val("");
   }
 });
 
-$("#delete-btn").on("click", function () {
-  // event.preventDefault();
-  console.log("delete-btn was pressed");
-  alert("Way to go!");
-});
+$(document).ready(function() {
+    $("#delete-btn").on("click", function () {
+        // event.preventDefault();
+        console.log("delete-btn was pressed");
+        alert("Way to go!");
+      });
+})
+
+let totalCost = [];
 
 let orderArr = [];
 
-window.addEventListener("load", function(){
-    // addCost()
-    
-});
-
-let addCost = () => {
-    console.log('addCost orderArr: ', orderArr)
-console.log(orderArr.length)
-    
-    // for (let i = 0; i < orderArr.length; i++) {
-    //     const element = orderArr[i];
-    //     console.log('element: ', element)
-    // }
+let addCost = (array) => {
+let sum = 0;
+for (let i = 0; i < array.length; i++) {
+    sum += array[i];
 }
+console.log('addCost sum: ', sum)
+$('#totalCostDisp').text("$" + sum)
+};
 
+let totalCostFun = () => {
+  console.log("totalCost: ", totalCost);
+};
 
+let retreive = (cb) => {
+  database.ref().on("child_added", function (childSnapshot) {
+    //   console.log('childSnapshot: ', childSnapshot);
 
-let retreive = () => {database.ref().on("child_added", function (childSnapshot,) {
-  //   console.log('childSnapshot: ', childSnapshot);
+    orderArr.push(childSnapshot.val());
+    //   console.log("orderArr: ", orderArr);
 
+    let vendor = childSnapshot.val().vendor;
+    let cost = childSnapshot.val().cost;
+    let orderDate = childSnapshot.val().date;
+    totalCost.push(parseInt(cost));
+    // console.log('retreive "Cost:" ',typeof parseInt(cost))
+    
+    addCost(totalCost)
 
+    let newOrderInfo = $("<tr>").append(
+      $("<td>").text(vendor),
+      $("<td>").text(cost),
+      $("<td>").text(orderDate),
+      $(
+        '<button key="' +
+          orderDate +
+          '" id="delete-btn" class="btn btn-primary">'
+      ).text("delete")
+    );
+    // console.log("newOrderInfo:", newOrderInfo);
+    $("#open-orders > tbody").append(newOrderInfo);
+    // addCost(totalCost);
+    cb;
+  });
+};
 
-  orderArr.push(childSnapshot.val());
-//   console.log("orderArr: ", orderArr);
+retreive(totalCostFun());
 
-  let vendor = childSnapshot.val().vendor;
-  let cost = childSnapshot.val().cost;
-  let orderDate = childSnapshot.val().date;
-
-  let newOrderInfo = $("<tr>").append(
-    $("<td>").text(vendor),
-    $("<td>").text(cost),
-    $("<td>").text(orderDate),
-    $(
-      '<button key="' + orderDate + '" id="delete-btn" class="btn btn-primary">'
-    ).text("delete")
-  );
-  console.log("newOrderInfo:", newOrderInfo);
-  $("#open-orders > tbody").append(newOrderInfo);
-  addCost()
-})};
-
-retreive()
+// console.log('totalCost: ', totalCost)
