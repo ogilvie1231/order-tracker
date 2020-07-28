45113a798203a6401d1e692dfe3601b2ab70e380
ogilvie1231 <ogilvie1231@gmail.com>
2020-07-28 07:31:01 -0600

    removing console logs and cleaning app.js

--- javascript/app.js
+++ javascript/app.js
@@ -32,7 +32,6 @@ $("#new-order-btn").on("click", function (event) {
   } else if (date === "") {
     alert("Please enter a date");
   } else {
-    // console.log('vendor: ', vendor + '\n' + 'cost: ', cost + '\n' + 'date: ', date)
 
     let newOrder = {
       vendor,
@@ -45,9 +44,6 @@ $("#new-order-btn").on("click", function (event) {
     database.ref().push(newOrder);
 
     window.location.reload();
-    // $("#vendor").val("");
-    // $("#cost").val("");
-    // $("#datepicker").val("");
   }
 });
 
@@ -64,7 +60,6 @@ let addCost = (array) => {
   for (let i = 0; i < array.length; i++) {
     sum += array[i];
   }
-  // console.log("addCost sum: ", sum);
   $("#totalCostDisp").text("$" + sum);
 };
 
@@ -75,42 +70,37 @@ let calcTax = (array) => {
     sum += array[i];
   }
   let taxAmount = (sum * taxPer).toFixed(2);
-  console.log('sum: ', sum)
-  console.log("calcTax tax amount: ", taxAmount);
   $("#totalTaxDisp").text("$" + taxAmount);
 };
 
 let completeBtn = (id) => {
   $("#" + id + "").on("click", function () {
-    // database.ref(id).remove()
     database.ref(id).update({
       complete: true,
     });
+    window.location.reload();
+  });
+};
 
-    console.log("delete-btn was pressed");
-    // alert("Way to go! finalId: " + id + " was pressed");
+let removeBtn = (id) => {
+  $("#" + id + "").on("click", function () {
+    database.ref(id).remove()
     window.location.reload();
   });
 };
 
 let deleteBtn = (id) => {
   $("#" + id + "").on("click", function () {
-    // database.ref(id).remove()
     database.ref(id).update({
       complete: false,
     });
-
-    console.log("delete-btn was pressed");
-    // alert("Way to go! finalId: " + id + " was pressed");
     window.location.reload();
   });
 };
 
-let retreive = (cb) => {
+let retreive = () => {
   database.ref().on("child_added", function (childSnapshot) {
-    console.log("childSnapshot.val(): ", childSnapshot.key);
     orderArr.push(childSnapshot.val());
-    // console.log("orderArr: ", orderArr);
 
     let vendor = childSnapshot.val().vendor;
     let cost = childSnapshot.val().cost;
@@ -118,34 +108,23 @@ let retreive = (cb) => {
     let itemKey = childSnapshot.key;
     let taxStat = childSnapshot.val().tax;
 
-    if (childSnapshot.val().tax === "Taxable") {
-      let cost = childSnapshot.val().cost
-      let taxStat = (cost * .56).toFixed(2);
-      // console.log('taxAmt: ', taxAmt)
-    }
-    
-    
     if (childSnapshot.val().complete === false) {
       totalCost.push(parseInt(cost));
       }
    
     addCost(totalCost);
 
-    // calcTax(taxableArr);
-
     if (childSnapshot.val().tax === "Taxable" && childSnapshot.val().complete === false) {
       taxableArr.push(parseInt(cost));
-      console.log("taxableArr: ", taxableArr);
     }
    
     if (childSnapshot.val().complete == false) {
-      // alert(childSnapshot.val().vendor + ' is false')
 
       let newOrderInfo = $("<tr>").append(
         $("<td>").text(vendor),
         $("<td>").text("$" + cost),
         $("<td>").text(orderDate),
-        $("<td>").text(moment(orderDate, "MM/DD/YYYY").fromNow()),
+        $("<td>").text(moment(orderDate, "MM/DD/YYYY").fromNow('d')),
         $("<td>").text(taxStat),
         $(
           '<button key="' +
@@ -157,8 +136,6 @@ let retreive = (cb) => {
       );
 
       $("#open-orders > tbody").append(newOrderInfo);
-      // addCost(totalCost);
-      cb;
 
       completeBtn(itemKey);
     } else if (childSnapshot.val().complete == true) {
@@ -176,25 +153,13 @@ let retreive = (cb) => {
             '" class="btn btn-primary delete-btn">'
         ).text("delete")
       );
-      // console.log("closedOrderInfo:", closedOrderInfo);
       $("#closed-orders > tbody").append(closedOrderInfo);
-      // addCost(totalCost);
-      cb;
 
       deleteBtn(itemKey);
     }
     calcTax(taxableArr);
   });
-  // deleteBtn(orderDate)
 };
 
 retreive();
 
-let counterFunc = () => {
-  let days = 0;
-  setInterval(function () {
-    // days++;
-    // console.log('days: ', days)
-    $("#timeCount").html(days++);
-  }, 1000);
-};
