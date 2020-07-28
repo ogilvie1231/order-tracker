aae7ab6709a952271333727262318583a431baf6
ogilvie1231 <ogilvie1231@gmail.com>
2020-07-27 22:12:54 -0600

    setting up taxable amount.

--- javascript/app.js
+++ javascript/app.js
@@ -11,12 +11,16 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 var database = firebase.database();
-//   firebase.analytics();
+
+let totalCost = [];
+let orderArr = [];
+let taxableArr = [];
 
 $("#new-order-btn").on("click", function (event) {
   let vendor = $("#vendor").val().trim();
   let cost = $("#cost").val().trim();
   let date = $("#datepicker").val().trim();
+  let tax = $("#taxOption").val().trim();
 
   event.preventDefault();
   if (vendor === "") {
@@ -35,14 +39,15 @@ $("#new-order-btn").on("click", function (event) {
       cost,
       date,
       complete: false,
+      tax,
     };
 
     database.ref().push(newOrder);
 
-    // window.location.reload();
-    $("#vendor").val("");
-    $("#cost").val("");
-    $("#datepicker").val("");
+    window.location.reload();
+    // $("#vendor").val("");
+    // $("#cost").val("");
+    // $("#datepicker").val("");
   }
 });
 
@@ -54,91 +59,136 @@ $(function () {
   });
 });
 
-let totalCost = [];
-
-let orderArr = [];
-
 let addCost = (array) => {
   let sum = 0;
   for (let i = 0; i < array.length; i++) {
     sum += array[i];
   }
-  console.log("addCost sum: ", sum);
+  // console.log("addCost sum: ", sum);
   $("#totalCostDisp").text("$" + sum);
 };
 
-let totalCostFun = () => {
-  console.log("totalCost: ", totalCost);
+let calcTax = (array) => {
+  let sum = 0;
+  let taxPer = 0.56;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  let taxAmount = (sum * taxPer).toFixed(2);
+  console.log('sum: ', sum)
+  console.log("calcTax tax amount: ", taxAmount);
+  $("#totalTaxDisp").text("$" + taxAmount);
+};
+
+let completeBtn = (id) => {
+  $("#" + id + "").on("click", function () {
+    // database.ref(id).remove()
+    database.ref(id).update({
+      complete: true,
+    });
+
+    console.log("delete-btn was pressed");
+    // alert("Way to go! finalId: " + id + " was pressed");
+    window.location.reload();
+  });
 };
 
 let deleteBtn = (id) => {
-  $("#" + id).on("click", function () {
-    // event.preventDefault();
+  $("#" + id + "").on("click", function () {
+    // database.ref(id).remove()
+    database.ref(id).update({
+      complete: false,
+    });
+
     console.log("delete-btn was pressed");
-    alert("Way to go!");
+    // alert("Way to go! finalId: " + id + " was pressed");
+    window.location.reload();
   });
 };
 
 let retreive = (cb) => {
   database.ref().on("child_added", function (childSnapshot) {
-    console.log("childSnapshot.val: ", childSnapshot.val());
-
+    console.log("childSnapshot.val(): ", childSnapshot.key);
     orderArr.push(childSnapshot.val());
-    console.log("orderArr: ", orderArr);
+    // console.log("orderArr: ", orderArr);
 
     let vendor = childSnapshot.val().vendor;
     let cost = childSnapshot.val().cost;
     let orderDate = childSnapshot.val().date;
-    totalCost.push(parseInt(cost));
-    // console.log('retreive "Cost:" ',typeof parseInt(cost))
+    let itemKey = childSnapshot.key;
+    let taxStat = childSnapshot.val().tax;
 
+    if (childSnapshot.val().tax === "Taxable") {
+      let cost = childSnapshot.val().cost
+      let taxStat = (cost * .56).toFixed(2);
+      // console.log('taxAmt: ', taxAmt)
+    }
+    
+    
+    if (childSnapshot.val().complete === false) {
+      totalCost.push(parseInt(cost));
+      }
+   
     addCost(totalCost);
 
-    let orderAge = moment("20200628", "MM/DD/YYYY").fromNow();
-    // let orderAge = moment().diff(orderDate)
-    console.log("orderAge: ", orderAge);
-    let dynamicId = "";
-    console.log("dynamicId: ", dynamicId);
+    // calcTax(taxableArr);
 
-    let newId = 0;
-    let newIdArr = orderDate.split("/");
-    for (let i = 0; i < newIdArr.length; i++) {
-      const elem = newIdArr[i];
-      newId += elem;
+    if (childSnapshot.val().tax === "Taxable" && childSnapshot.val().complete === false) {
+      taxableArr.push(parseInt(cost));
+      console.log("taxableArr: ", taxableArr);
     }
-    let finalId = newId + vendor;
-    // let dynamicId = finalId
-    console.log('finalId: ', finalId)
+   
+    if (childSnapshot.val().complete == false) {
+      // alert(childSnapshot.val().vendor + ' is false')
 
-    let newOrderInfo = $("<tr>").append(
-      $("<td>").text(vendor),
-      $("<td>").text(cost),
-      $("<td>").text(orderDate),
-      $("<td>").text(moment(orderDate, "MM/DD/YYYY").fromNow()),
-      //   console.log('orderDate:', orderDate),
-      $(
-        '<button key="' +
-          orderDate +
-          '" id="' +
-          finalId +
-          '" class="btn btn-primary delete-btn">'
-      ).text("delete")
-    );
-    // console.log("newOrderInfo:", newOrderInfo);
-    $("#open-orders > tbody").append(newOrderInfo);
-    // addCost(totalCost);
-    cb;
+      let newOrderInfo = $("<tr>").append(
+        $("<td>").text(vendor),
+        $("<td>").text("$" + cost),
+        $("<td>").text(orderDate),
+        $("<td>").text(moment(orderDate, "MM/DD/YYYY").fromNow()),
+        $("<td>").text(taxStat),
+        $(
+          '<button key="' +
+            itemKey +
+            '" id="' +
+            itemKey +
+            '" class="btn btn-primary delete-btn">'
+        ).text("Complete")
+      );
 
-    $("#" + finalId + "").on("click", function () {
-      // event.preventDefault();
-      console.log("delete-btn was pressed");
-      alert("Way to go! finalId: " + finalId +" was pressed");
-    });
+      $("#open-orders > tbody").append(newOrderInfo);
+      // addCost(totalCost);
+      cb;
+
+      completeBtn(itemKey);
+    } else if (childSnapshot.val().complete == true) {
+      let closedOrderInfo = $("<tr>").append(
+        $("<td>").text(vendor),
+        $("<td>").text("$" + cost),
+        $("<td>").text(orderDate),
+        $("<td>").text(moment(orderDate, "MM/DD/YYYY").fromNow()),
+        $("<td>").text(taxStat),
+        $(
+          '<button key="' +
+            orderDate +
+            '" id="' +
+            itemKey +
+            '" class="btn btn-primary delete-btn">'
+        ).text("delete")
+      );
+      // console.log("closedOrderInfo:", closedOrderInfo);
+      $("#closed-orders > tbody").append(closedOrderInfo);
+      // addCost(totalCost);
+      cb;
+
+      deleteBtn(itemKey);
+    }
+    calcTax(taxableArr);
   });
   // deleteBtn(orderDate)
 };
 
-retreive(deleteBtn());
+retreive();
 
 let counterFunc = () => {
   let days = 0;
